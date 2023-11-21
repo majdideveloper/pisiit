4eb61239509965b7fd16032955825b82a738e6c9
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-11-21 15:41:59 +0100

    nickname

---
+++ lib/features/auth/screens/signup_widget/widget_nickname.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pisiit/features/auth/widgets/customtext_field.dart';
+import 'package:pisiit/features/auth/widgets/widget_title.dart';
+import 'package:pisiit/utils/colors.dart';
+import 'package:pisiit/utils/helper_padding.dart';
+import 'package:pisiit/utils/helper_textstyle.dart';
+class NickNameWidget extends StatelessWidget {
+  final TabController tabController;
+   final TextEditingController nameController;
+  const NickNameWidget({super.key,
+   required this.tabController, required this.nameController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Column(
+        children: [
+        WidgetTitle(
+          title: 'Your Pesst identy 😎', 
+          subTitle: "Create a unique nickname that represents you it's how other will know and remember you"
+          ),
+          mediumPaddingHor,
+          CustomTextField(hintText: 'Nickname', controller: nameController,)
+      ]),
+    );
+  }
+}
