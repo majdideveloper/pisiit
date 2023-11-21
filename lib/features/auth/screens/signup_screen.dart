d4a9dd5a46126407e46f01ec97e3e1019479e8be
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-11-21 19:11:28 +0100

    relationgoals

--- lib/features/auth/screens/signup_screen.dart
+++ lib/features/auth/screens/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pisiit/features/auth/screens/signup_widget/widget_gender.dart';
 import 'package:pisiit/features/auth/screens/signup_widget/widget_nickname.dart';
+import 'package:pisiit/features/auth/screens/signup_widget/widget_relationgoals.dart';
 import 'package:pisiit/features/auth/widgets/textfield_auth.dart';
 import 'package:pisiit/utils/colors.dart';
 import 'package:pisiit/utils/helper_padding.dart';
@@ -16,67 +17,67 @@ class SignUpScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(),
         body: 
-       
-         Padding(
-           padding: const EdgeInsets.all(16.0),
-          child:  Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Create an account 👩‍💻",
-                  style: textStyleSubtitle,
-                ),
-                smallPaddingVert,
-                Text(
-                  "Create your account in Seconds. We'll help you find your perfect match",
-                  style: textStyleText,
-                ),
-                smallPaddingVert,
-                TextFieldAuth(
-                  nameTextField: "Email",
-                ),
-                smallPaddingVert,
-                TextFieldAuth(
-                  nameTextField: "Password",
-                  prefixIcon: const Icon(
-                    Icons.lock,
-                    color: blackColor,
-                  ),
-                  suffixIcon: const Icon(
-                    Icons.remove_red_eye,
-                    color: blackColor,
-                  ),
-                ),
-                mediumPaddingVert,
-               // ! Custom this in widget
-                Row(
-                  children: [
-                    Checkbox(value: false, onChanged: (value) {}),
-                    Text(
-                      'I agree to Pisit',
-                      style: textStyleText.copyWith(
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Privacy Policy',
-                        style: textStyleTextBold,
-                      ),
-                    )
-                  ],
-                ),
-                const Spacer(),
-                CustomButton(
-                  colorText: whiteColor,
-                  textButton: "Sign up",
-                  onPressed: () {},
-                ),
-                mediumPaddingVert,
-              ],
-            ),
-         )
+      
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+           child:  Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Text(
+                   "Create an account 👩‍💻",
+                   style: textStyleSubtitle,
+                 ),
+                 smallPaddingVert,
+                 Text(
+                   "Create your account in Seconds. We'll help you find your perfect match",
+                   style: textStyleText,
+                 ),
+                 smallPaddingVert,
+                 TextFieldAuth(
+                   nameTextField: "Email",
+                 ),
+                 smallPaddingVert,
+                 TextFieldAuth(
+                   nameTextField: "Password",
+                   prefixIcon: const Icon(
+                     Icons.lock,
+                     color: blackColor,
+                   ),
+                   suffixIcon: const Icon(
+                     Icons.remove_red_eye,
+                     color: blackColor,
+                   ),
+                 ),
+                 mediumPaddingVert,
+                 //! Custom this in widget
+                 Row(
+                   children: [
+                     Checkbox(value: false, onChanged: (value) {}),
+                     Text(
+                       'I agree to Pisit',
+                       style: textStyleText.copyWith(
+                         fontWeight: FontWeight.w900,
+                       ),
+                     ),
+                     TextButton(
+                       onPressed: () {},
+                       child: Text(
+                         'Privacy Policy',
+                         style: textStyleTextBold,
+                       ),
+                     )
+                   ],
+                 ),
+                 const Spacer(),
+                 CustomButton(
+                   colorText: whiteColor,
+                   textButton: "Sign up",
+                   onPressed: () {},
+                 ),
+                 mediumPaddingVert,
+               ],
+             ),
+          )
         );
   }
 }
