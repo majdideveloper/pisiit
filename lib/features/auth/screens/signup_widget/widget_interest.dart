0c477124ae69b80c440bfa3dc962f344518ee35e
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-11-22 10:30:37 +0100

    fix sign up

---
+++ lib/features/auth/screens/signup_widget/widget_interest.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class InterestWidget extends StatelessWidget {
+  final TabController tabController;
+  const InterestWidget({super.key, required this.tabController,});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
