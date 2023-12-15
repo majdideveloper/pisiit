137705273710cd0764e2fac381b8cf10125c14eb
Med Ali Krifa <daliolimona@gmail.com>
2023-12-15 10:05:11 +0100

    logout bottonsheet

--- lib/utils/custom_bottomsheet.dart
+++ lib/utils/custom_bottomsheet.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pisiit/features/auth/controller/auth_controller.dart';
 import 'package:pisiit/utils/colors.dart';
 import 'package:pisiit/utils/helper_padding.dart';
+import 'package:pisiit/utils/helper_textstyle.dart';
 import 'package:pisiit/widgets/custom_button.dart';
 
 class CustomBottomSheet extends ConsumerWidget {
@@ -14,36 +15,35 @@ class CustomBottomSheet extends ConsumerWidget {
         mainAxisSize: MainAxisSize.min,
         children: [
           Text(
-            'Elogout',
-            
+            'Logout',
+            style: textStyleSubtitle.copyWith(color: primaryColor),
           ),
- smallPaddingVert,
-          
-              (Row(
-                children: [
-                  Expanded(
-                    child: CustomButton(
-                    colorText: primaryColor,
-                    textButton: "cancel",
-                    color: lightColor,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  )),
-                  Expanded(
+          largePaddingVert,
+          Text("Are you sure you want to log out ?", style: textStyleTextBold),
+          mediumPaddingVert,
+          (Row(
+            children: [
+              Expanded(
                   child: CustomButton(
-                  colorText: whiteColor, textButton: "save",
-                  onPressed:(){
-                    ref.read(authControllerProvider).signOut(context);
-                  },
-                  
-                  )),
-                ],
-              ))
+                colorText: primaryColor,
+                textButton: "cancel",
+                color: lightColor,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )),
+              Expanded(
+                  child: CustomButton(
+                colorText: whiteColor,
+                textButton: "Yes,Logout?",
+                onPressed: () {
+                  ref.read(authControllerProvider).signOut(context);
+                },
+              )),
             ],
-          ),
-      
-      
+          ))
+        ],
+      ),
     );
   }
 }
